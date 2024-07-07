851c075df99a010eff8fceca613264bf853845e1
Annytech001 <godlybassey101@gmail.com>
2024-07-07 17:04:50 +0100

    Supabase updated

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+    children: React.ReactNode;
+};
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> =({
+    children
+}) => {
+    
+}
