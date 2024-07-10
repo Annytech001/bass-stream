7ed55efa3790284386587e752c35dd52ca3ed8cc
Annytech001 <godlybassey101>
2024-07-10 22:21:34 +0100

    layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "../Components/Side bar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <SupabaseProvider>
         <Sidebar>
           {children}
           </Sidebar>
+          </SupabaseProvider>
       </body>
     </html>
   )
