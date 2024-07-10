3d8710dfdd818c47f5f06299b8ef7c337a16aba0
Annytech001 <godlybassey101>
2024-07-10 23:11:12 +0100

    type ts update

--- types.ts
+++ types.ts
@@ -10,8 +10,28 @@ export interface UserDetails {
     payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 };
 
+export interface Product {
+    id: string;
+    active?: boolean;
+    name?: string;
+    description?: string;
+    image?: string;
+    metadata?: Stripe.Metadata;
+}
+
 export interface price {
-    
+    id: string;
+    product_id?: string;
+    active?: boolean;
+    description?: string;
+    unit_amount?: number;
+    currency?: string;
+    type?:Stripe.Price.Type;
+    interval?: Stripe.Price.Recurring.interval;
+    interval_count?: number;
+    trial_period_days?: number | null;
+    metadata?: Stripe.Metadata;
+    Products?: Product;
 }
 
 export interface Subcription {
@@ -21,7 +41,7 @@ export interface Subcription {
     metadata?: Stripe.metadata;
     price_id?: string;
     quantity?: string;
-    cancel_at_period_end?: Boolean;
+    cancel_at_period_end?: boolean;
     created: string;
     current_period_start: string;
     current_period_end: string;
